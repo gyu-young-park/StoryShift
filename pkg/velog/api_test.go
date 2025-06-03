d7c8e839eb0780ab6fc6c60c3650fe0a9b55cde4
gyu-young-park <gyoue200125@gmail.com>
2025-06-03 18:17:39 +0900

    chore: add github action to test push code into main branch

--- pkg/velog/api_test.go
+++ pkg/velog/api_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestReadSeriesAPIWhenSuccess(t *testing.T) {
 	velogApi := NewVelogAPI("https://v2.velog.io/graphql")
-	readSeries, err := velogApi.ReadSeries("CKA", "chappi")
+	readSeries, err := velogApi.ReadSeries("chappi", "CKA")
 
 	assert.NoError(t, err, "error occured")
 	t.Log(readSeries)
