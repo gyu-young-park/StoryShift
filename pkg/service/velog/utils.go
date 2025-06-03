bcdae37ee9fde93cf5daf6220356d866b7059298
gyu-young-park <gyoue200125@gmail.com>
2025-06-03 17:51:24 +0900

    feat: add feature that extracts picture url from velog post

--- pkg/service/velog/utils.go
+++ pkg/service/velog/utils.go
@@ -12,6 +12,13 @@ func sanitizeBasePathSpecialCase(filename string) (string, bool) {
 }
 
 func markdownPictureMatcher(contents string) []string {
-	// regexp.Compile("[]")
-	return []string{}
+	re := regexp.MustCompile(`!\[[^\]]*\]\(([^)]+)\)`)
+	matches := re.FindAllStringSubmatch(contents, -1)
+
+	pictures := []string{}
+	for _, match := range matches {
+		pictures = append(pictures, match[1])
+	}
+
+	return pictures
 }
--- pkg/service/velog/utils_test.go
+++ pkg/service/velog/utils_test.go
@@ -15,3 +15,13 @@ func TestSanitizeBasePathSpecialCaseWithSuccess(t *testing.T) {
 	assert.Equal(t, expect, sanitized)
 	assert.True(t, isSanitized)
 }
+
+func TestMarkdownPictureMatcher(t *testing.T) {
+	data := `c또는 Rust source code는 eBPF bytecode로 컴파일된다. 이 eBPF bytecode는 JIT compile되거나 interpreted되어 native machine code 명령어로 변환된다. 다음의 그림을 참고하자.  
+![](https://velog.velcdn.com/images/chappi/post/42f30731-7b86-4713-a429-3acc63d288a1/image.png)
+
+eBPF program은 eBPF bytecode 명령어 셋으로 assembly로 programming할 수 있지만, 사람이 읽을 수 있고, programming하기 좋은 c나 rust와 같은 언어로 먼저 작성하고 bytecode로 만들어 실행하는 것이 좋다. `
+
+	pictures := markdownPictureMatcher(data)
+	assert.Equal(t, "https://velog.velcdn.com/images/chappi/post/42f30731-7b86-4713-a429-3acc63d288a1/image.png", pictures[0])
+}
