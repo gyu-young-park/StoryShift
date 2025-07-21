0cd066b841cdd0fe46cc04f88682325452b281f6
gyu-young-park <gyoue200125@gmail.com>
2025-07-21 21:30:59 +0900

    feat: add user entity and gorm package

--- pkg/user/entity.go
+++ pkg/user/entity.go
@@ -1,1 +1,10 @@
 package user
+
+import "gorm.io/gorm"
+
+type UserEntity struct {
+	gorm.Model
+	Name    string `gorm:"type:varchar(100);not null"`
+	Email   string `gorm:"uniqueIndex;not null"`
+	VelogId string `gorm:"type:varchar(100);not null"`
+}
--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,1 +1,12 @@
 package user
+
+import "time"
+
+type User struct {
+	Id        string
+	Password  string
+	Email     string
+	VelogName string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
