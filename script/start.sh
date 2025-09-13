70a747d938c9ed0395e26304cbd7f28926c765b2
gyu-young-park <gyoue200125@gmail.com>
2025-09-13 23:50:24 +0900

    chore: add script to start cli or web server

--- script/start.sh
+++ script/start.sh
@@ -2,14 +2,38 @@
 
 PROJECT_ROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 CMD_DIR="${PROJECT_ROOT_PATH}/cmd"
-CMD_MAIN_FILE="${CMD_DIR}/web/main.go"
+CMD_WEB_MAIN_FILE="${CMD_DIR}/web/main.go"
+CMD_CLI_MAIN_FILE="${CMD_DIR}/cli/main.go"
+CMD=""
 
 export STORY_SHIFT_CONFIG_FILE="${PROJECT_ROOT_PATH}/config/test_config.yaml"
 
-echo "CMD File: ${CMD_MAIN_FILE}"
+function parse_opt() {
+    while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -c)
+        MODE="$2"
+        shift 2
+        ;;
+      *)
+        echo "Usage: $0 -c [web|cli]"
+        exit 1
+        ;;
+    esac
+  done
 
-start_cmd_main() {
-    go run $CMD_MAIN_FILE
+  case "$MODE" in
+    web) CMD=$CMD_WEB_MAIN_FILE ;;
+    cli) CMD=$CMD_CLI_MAIN_FILE ;;
+    *)   echo "Invalid or missing mode"; exit 1 ;;
+  esac
 }
 
-start_cmd_main
+function start_cmd_main() {
+    parse_opt "$@"
+
+    echo "CMD File: ${CMD}"
+    go run $CMD
+}
+
+start_cmd_main "$@"
